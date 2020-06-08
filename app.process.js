cf313c1a5992362393720c39805e53016a1b28af
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-06-08 11:12:24 +0700

    initial commit

---
+++ app.process.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+app.get('/', (req,res) => res.sendFile(path.join(__dirname + '/src/index.html')));
+app.use(express.static(__dirname + '/src'));
+app.listen(port, () => console.log(`Url shortener listening on post ${port}!`));
