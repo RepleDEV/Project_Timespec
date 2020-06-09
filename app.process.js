2050e8e7f411e8fea4d1160600760507c13f6664
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-06-09 10:47:17 +0700

    feature update and hotfix

--- app.process.js
+++ app.process.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-app.get('/', (req,res) => res.sendFile(path.join(__dirname + '/src/index.html')));
+
+const htmldir = "/src/html/";
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname + htmldir + 'homepage.index.html')));
+app.get('/timeline', (req,res) => res.sendFile(path.join(__dirname + htmldir + 'timeline.index.html')))
 app.use(express.static(__dirname + '/src'));
-app.listen(port, () => console.log(`Url shortener listening on post ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
--- src/js/homepage.script.js
+++ src/js/homepage.script.js
@@ -1,8 +1,9 @@
 const body = document.getElementsByTagName("body");
 
-var theme = 0;
+var theme = 1;
 function themeSwitch(n){
     body[0].className = body[0].className.replace(theme == 0 ? "light" : "dark", theme == 0 ? "dark" : "light");
     theme = theme == 0 ? 1 : 0;
     return;
 }
+
